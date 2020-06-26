62bc12f61c9be8c525e15abc74cd34c43d02020a
Alaa-Shaker <eng.alaashaker4@gmail.com>
2020-06-27 02:48:17 +0300

    Updates

---
+++ precache-manifest.63da10982466770442517300f3ac56dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662af6ed4c2c63d006ee31bce1d410d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99e2ac26d215fd7a480b",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "66f8652ccbce673d6b23",
+    "url": "/static/css/main.5c4486df.chunk.css"
+  },
+  {
+    "revision": "99e2ac26d215fd7a480b",
+    "url": "/static/js/2.00046f6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00046f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f8652ccbce673d6b23",
+    "url": "/static/js/main.de0fcc9f.chunk.js"
+  },
+  {
+    "revision": "7bfced2d2799b925a025",
+    "url": "/static/js/runtime-main.4622aba6.js"
+  },
+  {
+    "revision": "4100098c8af8b04589771aa58935f4c8",
+    "url": "/static/media/brain.4100098c.png"
+  }
+]);
